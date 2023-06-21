066fc69f2ecbaedd2d74b1e662ee3682503f1df2
reclamegraag <reclamegraag@gmail.com>
2023-06-21 15:32:27 +0200

    Fases and effects adjusted based on user feedback.

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app = Flask(__name__)
 app.jinja_env.filters['format_number'] = format_number
 
 # Define the fases and effects lists
-fasen = ['Alle fases', 'A1 - Productie: Winning grondstoffen', 'A2 - Productie: Transport', 'A3 - Productie: Productie', 'A4 - Bouw: Transport', 'A5 - Bouw: Bouw en installatie', 'B1 - Gebruik: Gebruik', 'B2 - Gebruik: Onderhoud', 'B3 - Gebruik: Reparaties', 'B4 - Gebruik: Vervangingen', 'B5 - Gebruik: Hernieuwing', 'B6 - Gebruik: Energie', 'B7 - Gebruik: Water', 'C1 - Sloop en verwerking: Sloop', 'C2 - Sloop en verwerking: Transport', 'C3 - Sloop en verwerking: Afvalbewerking', 'C4 - Sloop en verwerking: Finale afvalbewerking', 'D - Hergebruik en externe milieubaten -en lasten']
-effecten = ['Alle effecten', 'NOx - Stikstof', 'CO2 - Klimaatverandering', 'SO2 - Verzuring', 'PO4 - Vermesting', 'CFK-11 - Ozonlaagaantasting', 'C2H4 - Ethyleen smog', 'Uitputting brandstoffen', 'Uitputting natuurlijke hulpbronnen', 'Giftige stoffen zoetwater', 'Giftige stoffen zoutwater', 'Giftige stoffen vaste land', 'Giftige stoffen mensen']
+fasen = ['Alle fases', 'A1-A3, Productie', 'A1 - Productie: Winning grondstoffen', 'A2 - Productie: Transport', 'A3 - Productie: Productie', 'A4 - Bouw: Transport', 'A5 - Bouw: Bouw en installatie', 'B1 - Gebruik: Gebruik', 'B2 - Gebruik: Onderhoud', 'B3 - Gebruik: Reparaties', 'B4 - Gebruik: Vervangingen', 'B5 - Gebruik: Hernieuwing', 'B6 - Gebruik: Energie', 'B7 - Gebruik: Water', 'C1 - Sloop en verwerking: Sloop', 'C2 - Sloop en verwerking: Transport', 'C3 - Sloop en verwerking: Afvalbewerking', 'C4 - Sloop en verwerking: Finale afvalbewerking', 'D - Hergebruik en externe milieubaten -en lasten']
+effecten = ['MKI', 'MKI: CO2 - Klimaatverandering', 'MKI: SO2 - Verzuring', 'MKI: PO4 - Vermesting', 'MKI: CFK-11 - Ozonlaagaantasting', 'MKI: C2H4 - Ethyleen smog', 'MKI: Uitputting brandstoffen', 'MKI: Uitputting natuurlijke hulpbronnen', 'MKI: Giftige stoffen zoetwater', 'MKI: Giftige stoffen zoutwater', 'MKI: Giftige stoffen vaste land', 'MKI: Giftige stoffen mensen', 'NOx - Stikstof']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
