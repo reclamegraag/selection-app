59ce4738e7c88a8b45fa5a14da2aee711bd78b94
reclamegraag <reclamegraag@gmail.com>
2023-06-21 17:37:22 +0200

    Effects sorted in table and some renaming

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app = Flask(__name__)
 app.jinja_env.filters['format_number'] = format_number
 
 # Define the fases and effects lists
-fasen = ['Alle fases', 'A1-A3, Productie', 'A1 - Productie: Winning grondstoffen', 'A2 - Productie: Transport', 'A3 - Productie: Productie', 'A4 - Bouw: Transport', 'A5 - Bouw: Bouw en installatie', 'B1 - Gebruik: Gebruik', 'B2 - Gebruik: Onderhoud', 'B3 - Gebruik: Reparaties', 'B4 - Gebruik: Vervangingen', 'B5 - Gebruik: Hernieuwing', 'B6 - Gebruik: Energie', 'B7 - Gebruik: Water', 'C1 - Sloop en verwerking: Sloop', 'C2 - Sloop en verwerking: Transport', 'C3 - Sloop en verwerking: Afvalbewerking', 'C4 - Sloop en verwerking: Finale afvalbewerking', 'D - Hergebruik en externe milieubaten -en lasten']
-effecten = ['MKI', 'MKI: CO2 - Klimaatverandering', 'MKI: SO2 - Verzuring', 'MKI: PO4 - Vermesting', 'MKI: CFK-11 - Ozonlaagaantasting', 'MKI: C2H4 - Ethyleen smog', 'MKI: Uitputting brandstoffen', 'MKI: Uitputting natuurlijke hulpbronnen', 'MKI: Giftige stoffen zoetwater', 'MKI: Giftige stoffen zoutwater', 'MKI: Giftige stoffen vaste land', 'MKI: Giftige stoffen mensen', 'NOx - Stikstof']
+fasen = ['Alle fases', 'A1-A3: Productie', 'A1 - Productie: Winning grondstoffen', 'A2 - Productie: Transport', 'A3 - Productie: Productie', 'A4 - Bouw: Transport', 'A5 - Bouw: Bouw en installatie', 'B1 - Gebruik: Gebruik', 'B2 - Gebruik: Onderhoud', 'B3 - Gebruik: Reparaties', 'B4 - Gebruik: Vervangingen', 'B5 - Gebruik: Hernieuwing', 'B6 - Gebruik: Energie', 'B7 - Gebruik: Water', 'C1 - Sloop en verwerking: Sloop', 'C2 - Sloop en verwerking: Transport', 'C3 - Sloop en verwerking: Afvalbewerking', 'C4 - Sloop en verwerking: Finale afvalbewerking', 'D - Hergebruik en externe milieubaten -en lasten']
+effecten = ['MKI volledig', 'MKI: CO2 - Klimaatverandering', 'MKI: SO2 - Verzuring', 'MKI: PO4 - Vermesting', 'MKI: CFK-11 - Ozonlaagaantasting', 'MKI: C2H4 - Ethyleen smog', 'MKI: Uitputting brandstoffen', 'MKI: Uitputting natuurlijke hulpbronnen', 'MKI: Giftige stoffen zoetwater', 'MKI: Giftige stoffen zoutwater', 'MKI: Giftige stoffen vaste land', 'MKI: Giftige stoffen mensen', 'NOx - Stikstof']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,12 +34,13 @@ def index():
 def submit():
     data = request.form
     results = {}
-    selected_effects = set()
+    selected_effects = []
     for key, value in data.items():
         fase, effect = key.split('|')
         fase = fase.strip()
         effect = effect.strip()
-        selected_effects.add(effect)
+        if effect not in selected_effects:
+            selected_effects.append(effect)
         if fase not in results:
             results[fase] = {}
         results[fase][effect] = value
